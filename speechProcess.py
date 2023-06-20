c2556f514d3afdd7132c84f61bf674c02d3bed92
Tim <tim.scholtz@outlook.com>
2023-06-20 12:44:45 -0400

    Added analyse script which is able to take an audio file + expected output and analyses accuray of program

--- speechProcess.py
+++ speechProcess.py
@@ -144,231 +144,211 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 #Get a list of files paths to all audio files inside InputAudioData
 try:
-    nonDem = getFilesInFolder(dir_path+'/InputAudioData/Non_DementiaFiles')
+    input = getFilesInFolder(dir_path+'/InputAudioData')
 except:
-    print("Non dem Files not found")
-    nonDem=[]
-try:    
-    Dem = getFilesInFolder(dir_path+'/InputAudioData/DementiaFiles')
-except:
-    print("Dem Files not found")
-    Dem = []
+    print("No audio Files found")
+    input=[]
 
 #Create empty directorys for output data
 if os.path.exists(dir_path+'/output'):
     shutil.rmtree(dir_path+'/output')
 os.makedirs(dir_path+'/output')
-if os.path.exists(dir_path+'/output/non_dem_output'):
-    shutil.rmtree(dir_path+'/output/non_dem_output')
-os.makedirs(dir_path+'/output/non_dem_output')
-if os.path.exists(dir_path+'/output/dem_output'):
-    shutil.rmtree(dir_path+'/output/dem_output')
-os.makedirs(dir_path+'/output/dem_output')
-
-non_dem_out = "output/non_dem_output"
-dem_out = "output/dem_output"
-
-#runs through all files, and generates data
-loopData = [[nonDem,non_dem_out,'/InputAudioData/Non_DementiaFiles'],[Dem, dem_out,'/InputAudioData/DementiaFiles']]
 
 #Loop through all audio files and process them acoordingly
-for loop in loopData:
-    for audio in loop[0]:
-        file_name = os.path.basename(audio)
-        file = os.path.splitext(file_name)
-        file_location = audio.removeprefix(dir_path+loop[2])
-        file_type = file[1]
-        wavFile = dir_path+'/'+loop[1]+'/'+file[0]+"/"+file[0]+".wav"
-        os.makedirs(dir_path+'/'+loop[1]+'/'+file[0])
-        f = open(dir_path+'/'+loop[1]+'/'+file[0]+"/filedata.txt", "a")
-        csvGen = open(dir_path+'/'+loop[1]+'/'+file[0]+"/genData.csv", "a")
-        csvWriter = csv.writer(csvGen)
-        f.write("The file came from: "+ file_location)
+for audio in input:
+    file_name = os.path.basename(audio)
+    file = os.path.splitext(file_name)
+    file_location = audio.removeprefix(dir_path+'/InputAudioData')
+    file_type = file[1]
+    wavFile = dir_path+'/'+"output"+'/'+file[0]+"/"+file[0]+".wav"
+    os.makedirs(dir_path+'/'+"output"+'/'+file[0])
+    f = open(dir_path+'/'+"output"+'/'+file[0]+"/filedata.txt", "a")
+    csvGen = open(dir_path+'/'+"output"+'/'+file[0]+"/genData.csv", "a")
+    csvWriter = csv.writer(csvGen)
+    f.write("The file came from: "+ file_location)
 
-        #convert the file to a .wav if needed, and then copy the .wav to be part of the output
-        if(file_type == ".wav"):
-            if(verbose): print("Native Wave File:" + dir_path+'/'+loop[1]+'/'+file[0]+"/"+file_name)
-            ob = sf.SoundFile(audio)
-            if(format(ob.subtype) != "PCM_16"):
-                data, samplerate = sf.read(audio)
-                sf.write(dir_path+'/'+loop[1]+'/'+file[0]+"/"+file_name, data, samplerate, subtype='PCM_16')
-            else:
-                shutil.copyfile(audio, dir_path+'/'+loop[1]+'/'+file[0]+"/"+file_name)
+    #convert the file to a .wav if needed, and then copy the .wav to be part of the output
+    if(file_type == ".wav"):
+        if(verbose): print("Native Wave File:" + dir_path+'/'+"output"+'/'+file[0]+"/"+file_name)
+        ob = sf.SoundFile(audio)
+        if(format(ob.subtype) != "PCM_16"):
+            data, samplerate = sf.read(audio)
+            sf.write(dir_path+'/'+"output"+'/'+file[0]+"/"+file_name, data, samplerate, subtype='PCM_16')
+        else:
+            shutil.copyfile(audio, dir_path+'/'+"output"+'/'+file[0]+"/"+file_name)
 
-        elif(file_type == ".mp3"):
-            if(verbose): print("Converting Mp3 File:" + dir_path+'/'+loop[1]+'/'+file[0]+"/"+file_name)
-            sound = pydub.AudioSegment.from_mp3(audio)
-            sound.set_channels(1)
-            sound = sound.set_frame_rate(16000)                
-            sound = sound.set_channels(1)   
+    elif(file_type == ".mp3"):
+        if(verbose): print("Converting Mp3 File:" + dir_path+'/'+"output"+'/'+file[0]+"/"+file_name)
+        sound = pydub.AudioSegment.from_mp3(audio)
+        sound.set_channels(1)
+        sound = sound.set_frame_rate(16000)                
+        sound = sound.set_channels(1)   
+        sound.export(wavFile, format="wav")
+    elif(file_type == ".mp4"):
+        #may need to fix conversion - not sure if 16 bit wav or not
+        if(verbose): print("Converting Mp4 File:" + dir_path+'/'+"output"+'/'+file[0]+"/"+file_name)
+        video = moviepy.editor.VideoFileClip(audio, verbose=verbose ,)
+        #Extract the Audio
+        audio = video.audio
+        #Export the Audio
+        if(verbose):
+            audio.write_audiofile(wavFile)
+        else:
+            audio.write_audiofile(wavFile,logger=None)
+    elif(file_type == '.flac'):
+            data, samplerate = sf.read(audio)
+            sf.write(wavFile, data, samplerate, subtype='PCM_16')
+
+
+    conversion = AudioSegment.from_file(file=wavFile, format="wav")
+    n_channels = conversion.channels
+    if(n_channels>1):
+        try:
+            sound = AudioSegment.from_wav(wavFile)
+            sound = sound.set_channels(1)
             sound.export(wavFile, format="wav")
-        elif(file_type == ".mp4"):
-            #may need to fix conversion - not sure if 16 bit wav or not
-            if(verbose): print("Converting Mp4 File:" + dir_path+'/'+loop[1]+'/'+file[0]+"/"+file_name)
-            video = moviepy.editor.VideoFileClip(audio, verbose=verbose ,)
-            #Extract the Audio
-            audio = video.audio
-            #Export the Audio
-            if(verbose):
-                audio.write_audiofile(wavFile)
+        except:
+            print(colored("Error - trouble converting number of audio channels in file " + wavFile ,"red"))
+            sys.exit (1)
+
+
+    with wave.open(wavFile) as input:
+        #Read basic informaiton from the wavefile
+        f.write("\n"+"_"*15)
+        f.write("\nNumber of channels  - "+ str(n_channels) )
+        sample_width = input.getsampwidth()
+        f.write("\nSample Width - "+ str(sample_width))
+        sample_freq = input.getframerate()
+        f.write("\nFrame rate  - "+ str(sample_freq))
+        n_samples = input.getnframes()
+        f.write("\nNumber of Frames  - "+ str(n_samples))
+        t_audio = round(n_samples/sample_freq,3)
+        f.write("\nlenght of Audio sample - " + str(t_audio) )
+        f.write("\nCompression type  - "+ str(input.getcompname()) + " --- Orignal file type: "+ file_type)
+
+    #Break audio into 15 sec chuncks
+    folder =  dir_path+'/'+"output"+'/'+file[0]+"/"
+    file = file[0]+".wav"
+    split_wav = SplitWavAudioMubin(folder, file)
+    split_wav.multiple_split(min_per_split=1,sec_per_split=15)
+
+    split_files = split_wav.filepaths
+
+
+    # for every audio segment do the following
+    for seg in split_files:
+        
+        #file where data relating to audio seg should be stored
+        subfile = seg[:-4]
+
+        #audio file currently being processed
+        audioSeg = subfile+"/"+seg.rsplit('/', 1)[1]
+
+        
+        #file management
+        os.makedirs(subfile)
+        shutil.move(seg, audioSeg)
+
+        #Alosourus - get IPA data
+        outA_noTime = modelA.recognize(audioSeg)
+        outA_Time = modelA.recognize(audioSeg, timestamp=True)
+
+        with open( subfile+"/Alosourus_noTime.txt", "w") as text_file:
+            text_file.write(outA_noTime)
+
+        with open( subfile+"/Alosourus_Time.txt", "w") as text_file:
+            text_file.write(outA_Time)
+
+
+        #Vosk
+        wf = wave.open(audioSeg, "rb")
+        if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+            print("Audio file must be WAV format mono PCM.")
+            sys.exit(1)
+        rec = KaldiRecognizer(modelV, wf.getframerate())
+        rec.SetWords(True)
+        rec.SetPartialWords(True)
+        res = []
+        while True:
+            data = wf.readframes(4000)
+            if len(data) == 0:
+                break
+            if rec.AcceptWaveform(data):
+                res.append( json.loads(rec.Result()))
+                
             else:
-                audio.write_audiofile(wavFile,logger=None)
-        elif(file_type == '.flac'):
-                data, samplerate = sf.read(audio)
-                sf.write(wavFile, data, samplerate, subtype='PCM_16')
+                rec.PartialResult()
 
+        res.append( json.loads(rec.FinalResult()))
 
-        conversion = AudioSegment.from_file(file=wavFile, format="wav")
-        n_channels = conversion.channels
-        if(n_channels>1):
-            try:
-                sound = AudioSegment.from_wav(wavFile)
-                sound = sound.set_channels(1)
-                sound.export(wavFile, format="wav")
-            except:
-                print(colored("Error - trouble converting number of audio channels in file " + wavFile ,"red"))
-                sys.exit (1)
-
-
-        with wave.open(wavFile) as input:
-            #Read basic informaiton from the wavefile
-            f.write("\n"+"_"*15)
-            f.write("\nNumber of channels  - "+ str(n_channels) )
-            sample_width = input.getsampwidth()
-            f.write("\nSample Width - "+ str(sample_width))
-            sample_freq = input.getframerate()
-            f.write("\nFrame rate  - "+ str(sample_freq))
-            n_samples = input.getnframes()
-            f.write("\nNumber of Frames  - "+ str(n_samples))
-            t_audio = round(n_samples/sample_freq,3)
-            f.write("\nlenght of Audio sample - " + str(t_audio) )
-            f.write("\nCompression type  - "+ str(input.getcompname()) + " --- Orignal file type: "+ file_type)
-
-        #Break audio into 15 sec chuncks
-        folder =  dir_path+'/'+loop[1]+'/'+file[0]+"/"
-        file = file[0]+".wav"
-        split_wav = SplitWavAudioMubin(folder, file)
-        split_wav.multiple_split(min_per_split=1,sec_per_split=15)
-
-        split_files = split_wav.filepaths
-
-
-        # for every audio segment do the following
-        for seg in split_files:
-            
-            #file where data relating to audio seg should be stored
-            subfile = seg[:-4]
-
-            #audio file currently being processed
-            audioSeg = subfile+"/"+seg.rsplit('/', 1)[1]
-
-            
-            #file management
-            os.makedirs(subfile)
-            shutil.move(seg, audioSeg)
-
-            #Alosourus - get IPA data
-            outA_noTime = modelA.recognize(audioSeg)
-            outA_Time = modelA.recognize(audioSeg, timestamp=True)
-
-            with open( subfile+"/Alosourus_noTime.txt", "w") as text_file:
-                text_file.write(outA_noTime)
-
-            with open( subfile+"/Alosourus_Time.txt", "w") as text_file:
-                text_file.write(outA_Time)
-
-
-            #Vosk
-            wf = wave.open(audioSeg, "rb")
-            if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
-                print("Audio file must be WAV format mono PCM.")
-                sys.exit(1)
-            rec = KaldiRecognizer(modelV, wf.getframerate())
-            rec.SetWords(True)
-            rec.SetPartialWords(True)
-
-            while True:
-                data = wf.readframes(4000)
-                if len(data) == 0:
-                    break
-                if rec.AcceptWaveform(data):
-                    rec.Result()
-                else:
-                    rec.PartialResult()
-
-            res = json.loads(rec.FinalResult())
-            with open( subfile+"/vosk_transcipt.txt", "a+") as text_file:
-                text_file.write(res["text"]+"\n")
-
-            with open( subfile+"/vosk_timestamped.txt", "a+") as text_file:
+        with open(subfile+"/vosk_transcipt.txt", "w") as text_file:
+            text_file.write(" ")
+        with open(subfile+"/vosk_timestamped.txt", "w") as text_file:
+            text_file.write(" ")
+        with open( subfile+"/vosk_transcipt.txt", "a+") as text_file:
+            for result in res:
+                text_file.write(result["text"]+"\n")
+        with open( subfile+"/vosk_timestamped.txt", "a+") as text_file:
+            for result in res:
                 try:
-                    for line in res['result']:
+                    for line in result['result']:
                         text_file.write(str(line)+"\n")
                 except:
                     text_file.write("silence")
-            #Whisper AI - get text data 
-            
-            result_timestamped = modelW.transcribe(audioSeg, word_timestamps=True,language="en")
-         
-            with open( subfile+"/Whisper_transcipt.txt", "w") as text_file:
-                text_file.write(result_timestamped["text"])
+        #Whisper AI - get text data 
+        
+        result_timestamped = modelW.transcribe(audioSeg, word_timestamps=True,language="en")
+        
+        with open( subfile+"/Whisper_transcipt.txt", "w") as text_file:
+            text_file.write(result_timestamped["text"])
 
-            with open( subfile+"/whisper_timestamped.txt", "w") as text_file:
-                json_object = json.dumps(result_timestamped, indent=4)
-                text_file.write(json_object)
-            
-            #CMU Sphinx - I don't really know how this works, or how I got it to work
+        with open( subfile+"/whisper_timestamped.txt", "w") as text_file:
+            json_object = json.dumps(result_timestamped, indent=4)
+            text_file.write(json_object)
+        
+        #CMU Sphinx - I don't really know how this works, or how I got it to work
 
-            # Frames per Second
-            fps = 100
-            
-            # Decode streaming data
-            decoder = ps.Decoder(config)
+        # Frames per Second
+        fps = 100
+        
+        # Decode streaming data
+        decoder = ps.Decoder(config)
 
-            # Convert into 16KHz mono '.raw' file
-            y, sr = librosa.load(path=audioSeg, sr=16000, mono=True)
-            sf.write(file=(audioSeg[:-3]+'raw'), data=y, samplerate=sr, subtype='PCM_16', format='RAW')
-
-            
-            audioPS =AudioFile((audioSeg),frate=fps)
-            for phrase in audioPS:
-                for s in phrase.seg():
-                    with open( subfile+"/Cmu_transcipt.txt", "a+") as text_file:
-                        text_file.write(s.word + " ")
-
-                    with open( subfile+"/Cmu_timestamped.txt", "a+") as text_file:
-                        text_file.write(s.word + "," +  str(s.start_frame / fps) +","+str( s.end_frame / fps)+"\n" )
-                
-                with open( subfile+"/Cmu_transcipt.txt", "a+") as text_file:
-                        text_file.write("Confidence: "+ str(phrase.confidence()))
-
-                with open( subfile+"/Cmu_timestamped.txt", "a+") as text_file:
-                        text_file.write("Confidence: "+ str(phrase.confidence()))
-
-            decoder.start_utt()
-            stream = open(audioSeg[:-3]+'raw', 'rb')
-            while True:
-                buf = stream.read(1024)
-                if buf:
-                    decoder.process_raw(buf, False, False)
-                else:
-                    break
-            decoder.end_utt()
-
-            pho = [seg.word for seg in decoder.seg()]
-
-            with open( subfile+"/Cmu_phone.txt", "w") as text_file:
-                text_file.write(str(pho))
-
-            with open( subfile+"/Cmu_phone_ts.txt", "w") as text_file:
-                for seg in decoder.seg():
-                    text_file.write(str(seg.word + "," +str( seg.start_frame/fps) + ","+ str(seg.end_frame/fps) +"\n"))
-
-
-            
-       
-            
-
-
+        # Convert into 16KHz mono '.raw' file
+        y, sr = librosa.load(path=audioSeg, sr=16000, mono=True)
+        sf.write(file=(audioSeg[:-3]+'raw'), data=y, samplerate=sr, subtype='PCM_16', format='RAW')
 
         
+        audioPS =AudioFile((audioSeg),frate=fps)
+        for phrase in audioPS:
+            for s in phrase.seg():
+                with open( subfile+"/Cmu_transcipt.txt", "a+") as text_file:
+                    text_file.write(s.word + " ")
+
+                with open( subfile+"/Cmu_timestamped.txt", "a+") as text_file:
+                    text_file.write(s.word + "," +  str(s.start_frame / fps) +","+str( s.end_frame / fps)+"\n" )
+            
+            
+
+            with open( subfile+"/Cmu_timestamped.txt", "a+") as text_file:
+                    text_file.write("Confidence: "+ str(phrase.confidence()))
+
+        decoder.start_utt()
+        stream = open(audioSeg[:-3]+'raw', 'rb')
+        while True:
+            buf = stream.read(1024)
+            if buf:
+                decoder.process_raw(buf, False, False)
+            else:
+                break
+        decoder.end_utt()
+
+        pho = [seg.word for seg in decoder.seg()]
+
+        with open( subfile+"/Cmu_phone.txt", "w") as text_file:
+            text_file.write(str(pho))
+
+        with open( subfile+"/Cmu_phone_ts.txt", "w") as text_file:
+            for seg in decoder.seg():
+                text_file.write(str(seg.word + "," +str( seg.start_frame/fps) + ","+ str(seg.end_frame/fps) +"\n"))
+                
